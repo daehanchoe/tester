d217814e606a5f05cf555791537b61303afa6ecd
최대한 <choedaehan@choedaehan-ui-MacBook-Pro.local>
2021-01-12 23:55:20 +0900

    commit

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql');
+// const connection = mysql.createPool({
+// host: 'localhost',
+// port: 3306,
+// user: 'root',
+// password: 'mysql0616',
+// database: 'testuser'
+// });
+
+//
 const connection = mysql.createPool({
-host: 'localhost',
-port: 3306,
-user: 'root',
-password: 'mysql0616',
-database: 'testuser'
-});
+    host: 'database-3.cwakz2kcfjxd.ap-northeast-2.rds.amazonaws.com',
+    port: 3306,
+    user: 'tripsoda',
+    password: 'sodatrip2003',
+    database: 'tripsoda01'
+    });
 
 module.exports=connection;
